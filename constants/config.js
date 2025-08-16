e0115a46a3b95df734674e06da371444944b31dd
gauravpal74001 <palg74001@gmail.com>
2025-08-16 18:23:41 +0530

    fixing cors error

--- constants/config.js
+++ constants/config.js
@@ -3,6 +3,8 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:4173",
     process.env.CLIENT_URL,
+    "https://n-quick-talk-client.vercel.app",
+    "https://n-quick-talk-client-git-master-gauravs-projects-9c3630b4.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
